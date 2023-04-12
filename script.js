fb795bd71ece4c6e97677c77a7cf0dbcff98de54
kemontei <88424024+kemontei@users.noreply.github.com>
2023-04-12 19:01:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ async function main() {
     const data = await response.json();
 
     let abi = data.result;
-    console.log(abi);
+    //console.log(abi);
 
     const node = "https://eth.llamarpc.com";
     const provider = new ethers.providers.JsonRpcProvider(node);
@@ -13,7 +13,7 @@ async function main() {
     let privatekey = "fdfb72ce9754e3cbc1e79e44a8e20804cebd3c4a347605c6a3462a8de05b8784";
     let wallet = new ethers.Wallet(privatekey, provider);
 
-    console.log("Using wallet address " + wallet.address);
+    //console.log("Using wallet address " + wallet.address);
 
     let contractaddress = "0xFF9C1b15B16263C61d017ee9F65C50e4AE0113D7";
     let contract = new ethers.Contract(contractaddress, abi, wallet);
