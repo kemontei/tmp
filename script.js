9caf10d9ff0d4c6d70a9f52d8de85fe0f3b355dc
kemontei <88424024+kemontei@users.noreply.github.com>
2023-04-12 18:38:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const ethers = require("ethers");
 
 async function main() {
-    const response = await fetch('https://api-sepolia.etherscan.io/api?module=contract&action=getabi&address=0xFc7a5BD22dFc48565D6f04698E566Dd0C71d3155&apikey=YourApiKeyToken');
+    const response = await fetch('https://api.etherscan.io/api?module=contract&action=getabi&address=0xFF9C1b15B16263C61d017ee9F65C50e4AE0113D7&apikey=YourApiKeyToken');
     const data = await response.json();
 
     let abi = data.result;
     console.log(abi);
 
-    const node = "https://rpc.sepolia.org";
+    const node = "https://eth.llamarpc.com";
     const provider = new ethers.providers.JsonRpcProvider(node);
 
     let privatekey = "fdfb72ce9754e3cbc1e79e44a8e20804cebd3c4a347605c6a3462a8de05b8784";
@@ -15,11 +15,11 @@ async function main() {
 
     console.log("Using wallet address " + wallet.address);
 
-    let contractaddress = "0xFc7a5BD22dFc48565D6f04698E566Dd0C71d3155";
+    let contractaddress = "0xFF9C1b15B16263C61d017ee9F65C50e4AE0113D7";
     let contract = new ethers.Contract(contractaddress, abi, wallet);
 
     // calling the "retrieve" function to read the stored value
-    let read = await contract.read();
+    let read = await contract.totalSupply();
     console.log("Value stored in contract is " + read.toString());
 }
 
